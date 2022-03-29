5c60cf0689ffd98dc66297f01e92154a82258420
vo nam <nam.vo@ahasoft.vn>
2022-03-29 23:48:29 +0700

    update ui: update ui modal

--- src/components/modal/modal.vue
+++ src/components/modal/modal.vue
@@ -4,55 +4,62 @@
       {{ titleModal }}
     </h1>
     <div class="itemmodal__form">
-      <div class="form__item">
-        <label class="form__title">ITEM NAME</label>
-        <input v-model="itemName" class="form__input" type="text"/>
-      </div>
-      <div class="form__item">
-        <label class="form__title">FOOD CATEGORY</label>
-        <select class="form__select">
+      <b-row>
+        <b-col cols='6' class="form__item">
+          <label class="form__title">ITEM NAME</label>
+          <b-form-input v-model="itemName" class="form__input" type="text" />
+        </b-col>
+        <b-col cols='6' class="form__item">
+          <label class="form__title">FOOD CATEGORY</label>
+          <b-form-select class="form__select">
             <template v-for="(item, index) in categories">
-                <option :key="index" value="item">{{item}}</option>
+              <option :key="index" value="item">{{ item }}</option>
             </template>
-        </select>
-      </div>
-      <div class="form__item">
-        <label class="form__title">ORIGINAL QUANTITY</label>
-        <input class="form__input" type="text"/>
-      </div>
-      <div class="form__item">
-        <label class="form__title">QUANTITY LEFT</label>
-        <input class="form__input" type="text"/>
-      </div>
-      <div class="form__item">
-        <label class="form__title">EXPIRY DATE</label>
-        <input class="form__input" type="date-picker"/>
-      </div>
+          </b-form-select>
+        </b-col>
+        <b-col cols='6' class="form__item">
+          <label class="form__title">ORIGINAL QUANTITY</label>
+          <b-form-input class="form__input" type="number" />
+        </b-col>
+        <b-col cols='6' class="form__item">
+          <label class="form__title">QUANTITY LEFT</label>
+          <b-form-input class="form__input" type="number" />
+        </b-col>
+        <b-col cols='6' class="form__item">
+          <label class="form__title">EXPIRY DATE</label>
+          <b-form-datepicker :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }" class="form__input" locale="vi" />
+        </b-col>
+      </b-row>
     </div>
     <button class="btn" :class="buttonClass">Submit</button>
   </section>
 </template>
 <script>
-import {mapState} from 'vuex';
+import { mapState } from "vuex";
 export default {
   name: "modalComponent",
   data() {
-      return {
-          itemName: '',
-      }
+    return {
+      itemName: "",
+      itemCategory: '',
+      itemQuantity : 0,
+      itemQuantityLeft : 0,
+      itemExpire: ''
+    };
   },
   computed: {
     ...mapState({
-        categories: state => state.inventory.categories,
+      categories: (state) => state.inventory.categories,
     }),
     titleModal() {
       return "Add new item to inventory";
     },
     buttonClass() {
-        return "btn__add";
+      return "btn__add";
     },
   },
 };
 </script>
-<style lang="">
+<style lang="scss" scoped>
+@import "./modal.scss";
 </style>
